cbf61b42277d5c6931f2400096c63ef569fad666
hustcyb <hustcyb@gmail.com>
2018-05-09 23:47:54 +0800

    增加多环境配置

--- src/main/java/com/cyb/jetty/demo/controller/HomeController.java
+++ src/main/java/com/cyb/jetty/demo/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.cyb.jetty.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	@Value("${value}")
+	private String value;
+
 	@GetMapping("/")
 	public String home() {
 		return "Hello, World!";
+	}
+
+	@GetMapping("value")
+	public String value() {
+		return value;
 	}
 }
